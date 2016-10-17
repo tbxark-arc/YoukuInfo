a17daed31a26eb4d853baaa397aac51643550d9b
TBXark <vfanx@qq.com>
2016-10-17 12:59:23 +0800

    更新抓取逻辑

--- index.js
+++ index.js
@@ -35,60 +35,126 @@ router.post('/showdetail', function(req, res){
     var url = urls[index];
     if (url.length > 0 && url.indexOf("http://v.youku.com") >= 0) {
 
-        superagent.get(url)
-          .set("User-Agent", userAgent)
-          .end(function (err, sres) {
-            count = count - 1;
-            if (err) {
-              if (count == 0) {
-                res.send(content);
-              }
-              return ;
-            }
-          try {
-            var $ = cheerio.load(sres.text);
-            var titleRegx = new RegExp("<title>[^<]+</title>")
-            var rs1 = titleRegx.exec(sres.text);
-            var videoTitle = rs1[0].replace("<title>", "").replace("</title>", "").split("在线播放")[0];
-
-            var videoIdRegx = new RegExp("videoId2= '[a-zA-Z0-9=]*");
-            var rs2 = videoIdRegx.exec(sres.text);
-            var videoId = rs2[0].replace("videoId2= '", "");
-
-            var irTitle =  videoTitle
-            // var keywords =  $('meta[name="keywords"]').attr('content');
-            var videoCode = "<iframe height=498 width=510 src=\'http://player.youku.com/embed/" + videoId + "\' frameborder=0 \'allowfullscreen\'></iframe>";
-            // var image = $('#share-qq').attr('href').split('imageUrl=')[1].split('&')[0].replace("05420", "05410");
-            var image = $('#s_qq_haoyou1').attr('href').split('pics=')[1].split('&')[0].replace("05420", "05410").replace("r1", "r4");
-            var realUrl = "http://v.youku.com/v_show/id_" + videoId + ".html";
-
-
-            var jsonObj = {
-              url: url,
-              title: irTitle,
-              videoCode: videoCode,
-              image: image
-            };
-
-            var subContent =  "标题:" + "<br>"  + irTitle + "<a href='" + realUrl + "'>" + "[跳转网页]  " + "<a>"  + "<br>" + "<br>";
-            subContent = subContent + "缩略图:" + "<br>" + "<img src='"+image+"' />" + "<br>" + "<br>";
-            subContent = subContent + "通用代码:" + "<br>" + "<input id='videoCode' style='width:100%' value=\"" +  videoCode + "\"</input>" + "<br>" +"<br>" + "<br>" + "<br>";
-
-            content = content + subContent;
-
-            console.log( JSON.stringify(jsonObj, null, "\t") + "\n");
-
-          } catch (e) {
-            console.log("解析视频失败");
-          } finally {
-
-          }
-
+      superagent.get(url)
+        .set("User-Agent", userAgent)
+        .end(function (err, sres) {
+          if (err) {
             if (count == 0) {
               res.send(content);
             }
+            return ;
+          }
+
+          var numIdRegx = new RegExp('videoId:\"[a-zA-Z0-9=]*');
+          var rs0 = numIdRegx.exec(sres.text);
+
+          if (rs0 != null && rs0.length > 0) {
+              var numId = rs0[0].replace("videoId:\"", "");
+              var jsonURL = "http://play.youku.com/play/get.json?vid=" + numId + "&ct=12";
+              superagent.get(jsonURL)
+                .end(function(err, sres) {
+                  if (err) {
+                    if (count == 0) {
+                      res.send(content);
+                    }
+                    return ;
+                  }
+                  var respone = JSON.parse(sres.text);
+                  var realUrl = "http://v.youku.com/v_show/id_" + respone.data.video.encodeid + ".html";
+                  var videoCode = "<iframe height=498 width=510 src=\'http://player.youku.com/embed/" + respone.data.video.encodeid + "\' frameborder=0 \'allowfullscreen\'></iframe>";
+
+                  var jsonObj = {
+                    url: realUrl,
+                    title: respone.data.video.title,
+                    videoCode: videoCode,
+                    image: respone.data.video.logo
+                  };
+
+                  var subContent =  "标题:" + "<br>"  + jsonObj.title + "<a href='" + jsonObj.url + "'>" + "[跳转网页]  " + "<a>"  + "<br>" + "<br>";
+                  subContent = subContent + "缩略图:" + "<br>" + "<img src='"+jsonObj.image+"' />" + "<br>" + "<br>";
+                  subContent = subContent + "通用代码:" + "<br>" + "<input id='videoCode' style='width:100%' value=\"" +  jsonObj.videoCode + "\"</input>" + "<br>" +"<br>" + "<br>" + "<br>";
+
+                  content = content + subContent;
+
+                  console.log( JSON.stringify(jsonObj, null, "\t") + "\n");
+
+                  count = count - 1;
+                  if (count == 0) {
+                    res.send(content);
+                  }
+
+                });
+                return;
+          } else {
+            try {
+              var $ = cheerio.load(sres.text);
+              var titleRegx = new RegExp("<title>[^<]+</title>")
+              var rs1 = titleRegx.exec(sres.text);
+              var videoTitle = "";
+              if (rs1 != null && rs1.length > 0) {
+                videoTitle = rs1[0].replace("<title>", "").replace("</title>", "").split("在线播放")[0]
+              } else {
+                var titleRegx2 = new RegExp("showTitle =[^;]+;");
+                var rs1s =  titleRegx2.exec(sres.text);
+                if (rs1s != null && rs1s.length > 0) {
+                    videoTitle = rs1s[0].replace("showTitle =", "");
+                }
+              }
+
+              var videoIdRegx = new RegExp("(videoId2|videoIdEn)= '[a-zA-Z0-9=]*");
+              var rs2 = videoIdRegx.exec(sres.text);
+              var videoId = rs2[0].replace("(videoId2|videoIdEn)= '", "");
+
+              var irTitle =  videoTitle
+              // var keywords =  $('meta[name="keywords"]').attr('content');
+              var videoCode = "<iframe height=498 width=510 src=\'http://player.youku.com/embed/" + videoId + "\' frameborder=0 \'allowfullscreen\'></iframe>";
+
+              var image = "";
+              if ($('#s_qq_haoyou1').attr('href') != null) {
+                var data = $('#s_qq_haoyou1').attr('href').split('imageUrl=');
+                console.log("s_qq_haoyou1");
+                if (data != null && data.length > 0 ){
+                  console.log(data[1]);
+                  image = $('#s_qq_haoyou1').attr('href').split('imageUrl=')[1].split('&')[0].replace("05420", "05410").replace("r1", "r4");
+                }
+              } else if ($('#share-qq').attr('href') != null) {
+                console.log("share-qq");
+                var data = $('#share-qq').attr('href').split('pics=');
+                if (data != null && data.length > 0 ){
+                  console.log(data);
+                  image = data[1].split('&')[0].replace("05420", "05410").replace("r1", "r4");
+                }
+              }
+
+              var realUrl = "http://v.youku.com/v_show/id_" + videoId + ".html";
+
+              var jsonObj = {
+                url: url,
+                title: irTitle,
+                videoCode: videoCode,
+                image: image
+              };
+
+              var subContent =  "标题:" + "<br>"  + jsonObj.irTitle + "<a href='" + jsonObj.url + "'>" + "[跳转网页]  " + "<a>"  + "<br>" + "<br>";
+              subContent = subContent + "缩略图:" + "<br>" + "<img src='"+jsonObj.image+"' />" + "<br>" + "<br>";
+              subContent = subContent + "通用代码:" + "<br>" + "<input id='videoCode' style='width:100%' value=\"" +  jsonObj.videoCode + "\"</input>" + "<br>" +"<br>" + "<br>" + "<br>";
+
+              content = content + subContent;
+
+              console.log( JSON.stringify(jsonObj, null, "\t") + "\n");
+
+            } catch (e) {
+              console.log(e);
+            } finally {
+              count = count - 1;
+              if (count == 0) {
+                res.send(content);
+              }
+            }
+          }
           });
       } else {
+        console.log("不支持此URL");
         count = count - 1;
         if (count == 0) {
           res.send(content);
@@ -99,5 +165,5 @@ router.post('/showdetail', function(req, res){
 
 
 app.listen(3000, function () {
-  console.log('app is listening at port 3000');
+  console.log('App is listening at port 3000');
 });
